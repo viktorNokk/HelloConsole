7b4d4a95ee6d14affc97d71ec01f32964f894892
Kotov Viktor <V.Kotov@norsi-trans.lcl>
2022-08-19 17:49:48 +0300

    added Example 15

---
+++ Examples015_DirectoryRecursion/Program.cs
@@ -0,0 +1,33 @@
+﻿// Обход директорий
+
+// string path = "/Users/v.kotov/Desktop/HelloConsole/HelloConsole/Example001_HelloConsole";
+// DirectoryInfo di = new DirectoryInfo(path);
+// System.Console.WriteLine(di.CreationTime);
+// FileInfo[] fi = di.GetFiles();
+
+// for (int i = 0; i < fi.Length; i++)
+// {
+//     System.Console.WriteLine(fi[i].Name);
+// }
+
+void CatalogInfo(string path, string indent = "")
+{
+    DirectoryInfo catalog = new DirectoryInfo(path);
+
+    DirectoryInfo[] catalogs = catalog.GetDirectories();
+    for (int i = 0; i < catalogs.Length; i++)
+    {
+        Console.WriteLine($"{indent}{catalogs[i].Name}");
+        CatalogInfo(catalogs[i].FullName, indent + " ");
+    }
+    
+    FileInfo[] files = catalog.GetFiles();
+
+    for (int i = 0; i < files.Length; i++)
+    {
+        Console.WriteLine($"{indent}{files[i].Name}");
+    }
+}
+string path = "/Users/v.kotov/Desktop/HelloConsole/HelloConsole/Example001_HelloConsole";
+CatalogInfo(path);
+
