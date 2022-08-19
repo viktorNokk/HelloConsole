721a4f24e313d8f7bb046741e57179be9e476dfb
Kotov Viktor <V.Kotov@norsi-trans.lcl>
2022-08-19 18:46:55 +0300

    added new Example

---
+++ Examples016_RecursionContinue/Program.cs
@@ -0,0 +1,24 @@
+﻿// Игра "пирамидка"
+
+void Towers(string with = "1", string on = "3", string some = "2", int count = 3)
+{
+    if (count > 1) Towers(with, some, on, count - 1);
+    Console.WriteLine($"{with} >> {on}");
+    if (count > 1) Towers(some, on, with, count - 1);
+}
+
+Towers();
+
+// Обход деревьев
+string emp = String.Empty;
+string[] tree = {emp, "/", "*", "10", "-", "+", emp, emp, "4", "2", "1", "3"};
+//                0    1    2     3    4    5    6    7    8    9    10   11
+void InOrderTraversal(int pos = 1)
+{
+    if (pos < tree.Length)
+    {
+        int left = 2 * pos;
+        int right = 2 * pos + 1;
+        //if (left)
+    }
+}                
